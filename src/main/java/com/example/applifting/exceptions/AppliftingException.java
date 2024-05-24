c4a47c8aed53fdef98ee43057629016398b8001d
Zdenko Bučo <buco.zdeno@gmail.com>
2024-05-24 20:48:30 +0200

    add custom app exception

---
+++ src/main/java/com/example/applifting/exceptions/AppliftingException.java
@@ -0,0 +1,14 @@
+package com.example.applifting.exceptions;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class AppliftingException extends RuntimeException {
+    private Integer statusCode;
+    public AppliftingException(String message, int statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
